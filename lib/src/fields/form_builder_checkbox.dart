118a452c500580d2e1b5e8787700cbf786c8cea6
akhilSignzy <akhil.karnakoti@signzy.com>
2024-07-18 16:10:03 +0530

    Update form_builder_checkbox.dart

--- lib/src/fields/form_builder_checkbox.dart
+++ lib/src/fields/form_builder_checkbox.dart
@@ -129,6 +129,12 @@ class FormBuilderCheckbox extends FormBuilderFieldDecoration<bool> {
                 dense: true,
                 isThreeLine: false,
                 title: title,
+                fillColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+                     if (states.contains(MaterialState.selected)) {
+                            return  activeColor; 
+                        }
+                          return Color(0xFFFFFFFF);}
+                ),
                 subtitle: subtitle,
                 value: tristate ? state.value : (state.value ?? false),
                 onChanged: state.enabled
